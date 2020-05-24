39fd1944c5fe2b6c2679047a0ec58da5f420133e
Taj Deluca <me@tajdeluca.com>
2020-05-24 01:46:23 +0100

    Making sure footer date increments each year

--- src/app/modules/ui/components/footer/footer.component.ts
+++ src/app/modules/ui/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  today: number = Date.now();
+
   constructor() { }
 
   ngOnInit(): void {
