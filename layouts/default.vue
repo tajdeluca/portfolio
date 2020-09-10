d76d876500f56a2456f76d6c6597c013902241fa
Taj Deluca <me@tajdeluca.com>
2020-09-10 21:00:34 +0100

    Setting up a 404 page - It's a bit nicer, you know...

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="parent">
+    <div class="content">
+      <Nuxt />
+    </div>
     <footer>
       <small>Copyright &copy; Taj Deluca 2020</small>
     </footer>
@@ -8,6 +10,16 @@
 </template>
 
 <style scoped>
+.parent {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+
+.parent .content {
+  flex-grow: 1;
+}
+
 footer {
   padding: 1rem;
   text-align: center;
