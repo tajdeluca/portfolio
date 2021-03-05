03bee3645608567c8d18c72809a86a9fdfcfeafd
Taj Deluca <me@tajdeluca.com>
2021-03-05 18:41:06 +0000

    Fixing the dev build - For some reason require and hooks don't work so well...

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,6 +79,8 @@ export default {
               .fetch();
 
             document.categories = fullCategories;
+          } else {
+            document.categories = null;
           }
         }
       }
