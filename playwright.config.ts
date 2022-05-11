684e9f4ae1c7cdd60451a5ed67e2f2ef73af9c2c
Taj Deluca <me@tajdeluca.com>
2022-05-11 23:44:20 +0100

    Replacing cypress tests with playwright ones

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig, WebServerConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 /**
@@ -6,6 +6,11 @@ import { devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
+
+const featureWebServerConfig: WebServerConfig = {
+  command: 'npm start',
+  port: 3000
+};
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -34,7 +39,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.TEST_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -96,10 +101,7 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: process.env.TEST_IS_FEATURE_BRANCH ? featureWebServerConfig : undefined,
 };
 
 export default config;
